1f53dd97743d35f021de330ce11b5932031bfbf6
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-19 00:37:40 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,20 +3,21 @@ game 'gta5'
 
 author 'HenkW' 
 description 'ESX Admin Performance Checker' 
-version '1.0.0' 
+version '1.0.1' 
 
 
 dependency 'es_extended'
 
 server_scripts {
-    '@es_extended/locale.lua', 
-    'config.lua', 
-    'server.lua' 
+    'server/@es_extended/locale.lua', 
+    'server/config.lua', 
+    'server/server.lua' ,
+    'server/version.lua'
 }
 
 client_scripts {
-    '@es_extended/locale.lua', 
-    'config.lua',
-    'client.lua' 
+    'client/@es_extended/locale.lua', 
+    'client/config.lua',
+    'client/client.lua' 
 }
 
